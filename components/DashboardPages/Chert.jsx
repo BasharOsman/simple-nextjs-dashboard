a5d3cec8c66e6d04070a56ccd309512e3248342d
bashar <basharxosman@gmail.com>
2020-08-15 16:13:56 +0300

    simple pages for routes

---
+++ components/DashboardPages/Chert.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Chert extends Component {
+    render() {
+        return (
+            <div>
+                Chert Page
+            </div>
+        )
+    }
+}
